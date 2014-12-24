d373610aacf7fe29327b1a4716ae3b8b4891b121
Brock York <twunknown(add at here)gmail.com>
2014-12-24 12:55:04 +0800

    Defines the aspect ratio and field of view which is used to init the projection matrix which is passed into the shaders for use. Also now generates a model matrix for moving the box into place. Still needs a camera matrix

--- Main.cpp
+++ Main.cpp
@@ -6,32 +6,90 @@
 
 #include "Shader.h"
 #include "MD2Model.h"
+#include "Vec4.h"
+#include "Mat4.h"
 
+#include "Box.h"
+
+const GLfloat PI = 3.14;
 enum vaoIds {TRIANGLES, NUM_VAOS};
 enum bufferIds {ARRAY_BUFFER, NUM_BUFFERS};
-enum attribIds {vPosition = 0};
+enum indexIds{INDEXS, NUM_INDEXS};
+//enum attribIds {vPosition = 0};
+GLint vPosition;
+GLint vColour;
 
 GLuint vertArrays[NUM_VAOS];
 GLuint buffers[NUM_BUFFERS];
-const GLuint NUM_VERTICES = 6;
+GLuint ibo[NUM_INDEXS];
+const GLuint NUM_VERTICES = 8;
 
-GLint rotStepLocation;
+const GLfloat mf_aspectRatio = 1.0f / 1.0f;
+const GLfloat mf_fov = 90.0f;
+const GLfloat mf_near = 1;
+const GLfloat mf_far = 500;
+GLint projMatLocation;
+GLint modelMatLocation;
+
+Mat4<float> modelMatrix;
+
+float toRadians(float deg)
+{
+        return deg * (PI / 180.0f);
+}
 
 void init()
 {
-        GLfloat vertices[NUM_VERTICES][2] = {{-0.50, -0.50}, //Triangle 1
-                                                                           {0.50, -0.50},
-                                                                           {-0.50, 0.50},
-                                                                           {0.50, -0.50}, //Triangle 2
-                                                                           {0.50, 0.50},
-                                                                           {-0.50, 0.50}};
+        Vec4<float> min(-0.5, -0.5, -0.5, 1.0f);
+        Vec4<float> max(0.5, 0.5, 0.5, 1.0f);
+        Box b(min, max);
+
+        b.print();
+
+        glEnable(GL_MULTISAMPLE);
+        GLfloat tanHalfAngleRad = tan(toRadians(mf_fov / 2));
+        GLfloat nf = (mf_near + mf_far) / (mf_near - mf_far);
+        Vec4<GLfloat> col1(1.0f / tanHalfAngleRad, 0.0f, 0.0f, 0.0f);
+        Vec4<GLfloat> col2(0.0f, mf_aspectRatio / tanHalfAngleRad, 0.0f, 0.0f);
+        Vec4<GLfloat> col3(0.0f, 0.0f, nf, -1.0f);
+        Vec4<GLfloat> col4(0.0f, 0.0f, nf, 0.0f);
+
+        Mat4<GLfloat> projectionMatrix(col1, col2, col3, col4);
+
+        GLfloat colours[NUM_VERTICES][3] = {{1.0, 0.0, 0.0},
+                                                                          {0.0, 1.0, 0.0},
+                                                                          {0.0, 0.0, 1.0},
+                                                                          {1.0, 1.0, 1.0},
+                                                                          {1.0, 0.0, 0.0},
+                                                                          {0.0, 1.0, 0.0},
+                                                                          {0.0, 0.0, 1.0},
+                                                                          {1.0, 1.0, 1.0}};
+
+        GLushort indices[] = {0, 1, 2, //Front face
+                                     2, 3, 0,
+                                     5, 4, 7, //Back Face
+                                     7, 6, 5,
+                                     1, 5, 6, //Right side
+                                     6, 2, 1,
+                                     4, 0, 3, //left side
+                                     3, 7, 4,
+                                     3,2, 6, //Top
+                                     6, 7 ,3,
+                                     4, 5, 1,//Bottom
+                                     1, 0, 4};
+
+        glGenBuffers(NUM_INDEXS, ibo);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo[INDEXS]);
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
         glGenVertexArrays(NUM_VAOS, vertArrays);
         glBindVertexArray(vertArrays[TRIANGLES]);
 
         glGenBuffers(NUM_BUFFERS, buffers);
         glBindBuffer(GL_ARRAY_BUFFER, buffers[ARRAY_BUFFER]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+        glBufferData(GL_ARRAY_BUFFER, b.vsize() + sizeof(colours), NULL, GL_STATIC_DRAW);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, b.vsize(), b.Vertices());
+        glBufferSubData(GL_ARRAY_BUFFER, b.vsize(), sizeof(colours), colours);
 
         std::vector<struct ShaderList> list;
         list.push_back((struct ShaderList){GL_VERTEX_SHADER, "./shader.vert"});
@@ -40,40 +98,64 @@ void init()
         GLuint program = Shader::LoadShaders(list);
         glUseProgram(program);
 
-        glVertexAttribPointer(vPosition, 2, GL_FLOAT,  GL_FALSE, 0, (GLvoid*)0);
+        vPosition = glGetAttribLocation(program, "vPosition");
+        if(vPosition == -1)
+        {
+                std::cout << "Couldn't find vPosition in shader" << std::endl;
+        }
+
+        glVertexAttribPointer(vPosition, 3, GL_FLOAT,  GL_FALSE, 0, (GLvoid*)0);
         glEnableVertexAttribArray(vPosition);
 
-        rotStepLocation = glGetUniformLocation(program, "rotStep");
+        vColour = glGetAttribLocation(program, "vColour");
+        if(vColour == -1)
+        {
+                std::cout << "Couldn't find Colour  n in shader" << std::endl;
+        }
 
-        GLfloat pointSize = 0.0f;
-        glGetFloatv(GL_POINT_SIZE, &pointSize);
-        std::cout << "Point Size:" << pointSize << std::endl;
+        glVertexAttribPointer(vColour, 3, GL_FLOAT, GL_FALSE, 0, (const GLvoid*)b.vsize());
+        glEnableVertexAttribArray(vColour);
 
-        GLfloat pointRange[2];
-        glGetFloatv(GL_POINT_SIZE_RANGE, pointRange);
-        std::cout << "Point Range from " << pointRange[0] << " to " << pointRange[1] << std::endl;
+        projMatLocation = glGetUniformLocation(program, "vprojectionMat");
+        if(projMatLocation == -1)
+        {
+                std::cout << "Couldn't find vprojectionMat in shader" << std::endl;
+        }
 
-        glPointSize(20.0f);
+        modelMatLocation = glGetUniformLocation(program, "modelMatrix");
+        if(modelMatLocation == -1)
+        {
+                std::cout << "Couldn't find modelMatrix in shader" << std::endl;
+        }
+
+        glUniformMatrix4fv(projMatLocation, 1, false, &projectionMatrix);
 
         glCullFace(GL_BACK);
         glEnable(GL_CULL_FACE);
 
 }
 
-void draw(int rotStep)
+void draw()
 {
-        glUniform1i(rotStepLocation, rotStep);
+        glUniformMatrix4fv(modelMatLocation, 1, false, &modelMatrix);
 
         //std::cout << rotStep << std::endl;
         glClear(GL_COLOR_BUFFER_BIT);
 
         glBindVertexArray(vertArrays[TRIANGLES]);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo[INDEXS]);
 
-        glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+        //glDrawArrays(GL_TRIANGLES, 0, NUM_VERTICES);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_SHORT, NULL);
 }
 
 int main(int argc, char *argv[])
 {
+        modelMatrix = Mat4<float>(1.0f);
+
+        Mat4<float> transMatrix(1.0f);
+        transMatrix[2][3] = -2.5f;
+
         MD2Model skel;
         skel.loadModel("./hueteotl/tris.md2");
         SDL_Window *window = NULL;
@@ -88,6 +170,7 @@ int main(int argc, char *argv[])
                         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
                         SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
                         SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+                        SDL_GL_SetAttribute(SDL_GL_MULTISAMPLESAMPLES, 4);
                         context = SDL_GL_CreateContext(window);
                         if(context != NULL)
                         {
@@ -96,6 +179,12 @@ int main(int argc, char *argv[])
                                 SDL_GL_GetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, &major);
                                 SDL_GL_GetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, &minor);
                                 printf("Opengl context version %d.%d\n", major, minor);
+
+                                int samples;
+                                SDL_GL_GetAttribute(SDL_GL_MULTISAMPLESAMPLES, &samples);
+                                printf("Multisamples %d\n", samples);
+
+                                glViewport(0, 0, 640, 480);
 
                                 SDL_Event event;
 
@@ -120,7 +209,17 @@ int main(int argc, char *argv[])
                                                 }
                                         }
 
-                                        draw(rotStep);
+                                        //modelMatrix = Mat4<float>(1.0f); //Reset model matrix to the identity matrix
+                                        Mat4<float> rotMatrix(1.0f);
+                                        float radians = (float)rotStep * (PI / 180.0f);
+                                        rotMatrix[1][1] = cos(radians);
+                                        rotMatrix[1][2] = -sin(radians);
+                                        rotMatrix[2][1] = sin(radians);
+                                        rotMatrix[2][2] = cos(radians);
+
+                                        modelMatrix = rotMatrix * transMatrix;
+
+                                        draw();
 
                                         rotStep += 1;
                                         rotStep %= 360;
@@ -148,10 +247,4 @@ int main(int argc, char *argv[])
         }
 
         return 0;
-}
-
-bool glInit()
-{
-        bool retval = false;
-
 }
