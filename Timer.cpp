71e949d8b1fb4c95ddbb92aa7aa962e515ec402d
Brock York <twunknown@gmail.com>
2014-12-26 19:40:13 +0800

    Added my sdl timer class so camera movement is time based not fps based

---
+++ Timer.cpp
@@ -0,0 +1,93 @@
+#include "Timer.h"
+
+//Initialises the variables
+Timer::Timer()
+{
+    //ctor
+}
+
+Timer::~Timer()
+{
+    //dtor
+}
+
+/*Check if the timer hasnt already been started
+if so do nothing else set the start ticks to the current
+ticks and set started to true and paused to false*/
+void Timer::start( void )
+{
+    startTicks = SDL_GetTicks();
+    started = true;
+    paused = false;
+
+}
+
+//Set startTicks to zero started to false and paused to false
+void Timer::stop( void )
+{
+    if( started == true )
+    {
+        startTicks = 0;
+        started = false;
+        paused = false;
+    }
+
+}
+
+//check if not paused and if started if so Set paused ticks to the current ticks subtract startTicks then set the started ticks to zero
+//paused to true and started to true
+void Timer::pause( void )
+{
+    if( (started == true) && (paused == false) )
+    {
+            paused = true;
+
+            //gets the difference between when the timer started
+            //and when it got paused so we can "reset" the startTicks to the
+            //same distance from the current ticks
+            pausedTicks = SDL_GetTicks() - startTicks;
+    }
+}
+
+//Check the timer has started and is paused set the start ticks to
+//current ticks subtract paused ticks set paused to false and pausedTicks to 0
+void Timer::unpause( void )
+{
+    if( (started == true) && (paused == true) )
+    {
+        paused = false;
+
+        startTicks = SDL_GetTicks() - pausedTicks;
+
+        pausedTicks = 0;
+    }
+}
+
+Uint32 Timer::getTicks( void )
+{
+    if( started == true )
+    {
+        if( paused == true )
+        {
+            return pausedTicks;
+        }
+        else
+        {
+            return SDL_GetTicks() - startTicks;
+        }
+    }
+
+    return 0;
+}
+
+//Check if the timer is paused
+bool Timer::isPaused( void )
+{
+    return paused;
+}
+
+//Check if the timer has started
+bool Timer::isStarted( void )
+{
+    return started;
+}
---
+++ Timer.h
@@ -0,0 +1,53 @@
+#ifndef TIMER_H
+#define TIMER_H
+
+#include "SDL2/SDL.h"
+
+
+class Timer
+{
+
+    private:
+
+        //if the timer has started
+        bool started;
+
+        //if the timer is paused
+        bool paused;
+
+        //Ticks when the timer began
+        int startTicks;
+
+        //Ticks when the timer was paused
+        int pausedTicks;
+
+
+    public:
+
+        Timer( void );
+
+        virtual ~Timer( void );
+
+        //Start the timer
+        void start( void );
+
+        //Stop the timer
+        void stop( void );
+
+        //Pause the timer
+        void pause( void );
+
+        //Unpause the timer
+        void unpause( void );
+
+        //get the current ticks
+        Uint32 getTicks( void );
+
+        //Check if the timer is paused
+        bool isPaused( void );
+
+        //Check if the timer has started
+        bool isStarted( void );
+};
+
+#endif // TIMER_H
