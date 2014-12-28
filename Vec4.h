f4e8e158e37706a50fb88e1a1db02d61be3c03da
Brock York <twunknown(add at here)gmail.com>
2014-12-28 18:20:06 +0800

    Vector array is no longer hard coded to 4 instead it uses a constant variable Implemented unary - because it's needed in the camera object to negate a vector Implemented length, normalisation and crossproduct between vectors and can print a vector out

--- Vec4.h
+++ Vec4.h
@@ -1,22 +1,30 @@
 #ifndef VEC4_H
 #define VEC4_H
 
+#include <iostream>
+
 enum v
 {
         x = 0,
         y = 1,
-        z = 2
+        z = 2,
+        w = 3
 };
 
 template<typename T>
 class Vec4
 {
 private:
-        T xyzw[4];
+        static const int count = 4;
+        T xyzw[count];
 
 public:
         Vec4<T>()
         {
+                xyzw[0] = (T)0.0f;
+                xyzw[1] = (T)0.0f;
+                xyzw[2] = (T)0.0f;
+                xyzw[3] = (T)0.0f;
         }
 
         Vec4<T>(T x, T y, T z, T w)
@@ -35,6 +43,10 @@ public:
                 xyzw[3] = in.xyzw[3];
         }
 
+        Vec4<T> operator-()
+        {
+                return Vec4<T>(-xyzw[x], -xyzw[y], -xyzw[z], -xyzw[w]);
+        }
         T* operator&()
         {
                 return xyzw;
@@ -43,6 +55,51 @@ public:
         T &operator[](int index)
         {
                 return xyzw[index];
+        }
+
+        T length() const
+        {
+                T sum;
+                for(int i = 0; i < count; i++)
+                {
+                        sum += xyzw[i] * xyzw[i];
+                }
+
+                return sqrt(sum);
+        }
+
+        Vec4<T> normalise() const
+        {
+                T len = this->length();
+                return Vec4<T>(xyzw[x] / len, xyzw[y] / len, xyzw[z] / len, xyzw[w] / len);
+        }
+
+        Vec4<T> crossProduct( Vec4<T> &other)
+        {
+                Vec4<T> ortho;
+
+                ortho[x] = xyzw[y] * other[z] - xyzw[z] * other[y];
+                ortho[y] = xyzw[z] * other[x] - xyzw[x] * other[z];
+                ortho[z] = xyzw[x] * other[y] - xyzw[y] * other[x];
+
+                return ortho;
+        }
+
+        /*Vec4<T> operator*(Mat4<T> *m)
+        {
+                Vec4<T> retval;
+
+                retval[x] = xyzw[x] * m[0][0] + xyzw[y] * m[1][0] + xyzw[z] * m[2][0] + xyzw[w] * m[3][0];
+                retval[y] = xyzw[x] * m[0][1] + xyzw[y] * m[1][1] + xyzw[z] * m[2][1] + xyzw[w] * m[3][1];
+                retval[z] = xyzw[x] * m[0][2] + xyzw[y] * m[1][2] + xyzw[z] * m[2][2] + xyzw[w] * m[3][2];
+                retval[w] = xyzw[x] * m[0][3] + xyzw[y] * m[1][3] + xyzw[z] * m[2][3] + xyzw[w] * m[3][3];
+
+                return retval;
+        }*/
+
+        void print()
+        {
+                std::cout << "x:" << xyzw[x] << " y:" << xyzw[y] << " z:" << xyzw[z] << " w:" << xyzw[w] << std::endl;
         }
 };
 #endif // VEC4_H
