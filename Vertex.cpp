11e0e50473a36d73f466c414ae037a6d399eadf2
Brock York <twunknown@gmail.com>
2015-04-14 19:00:50 +1000

    Implemented Vertex class so that Vertex normal averaging is muchj easier to implement Normal averaging is achieved by adding every normal associated with the vertex to the object and then adding all the normals together and then normalising the normal

--- Vertex.cpp
+++ Vertex.cpp
@@ -1,11 +1,74 @@
 #include "Vertex.h"
+#include <cmath>
 
 Vertex::Vertex()
 {
-        //ctor
+        xyz.first = 0.0f;
+        xyz.second = 0.0f;
+        xyz.third = 0.0f;
+
+        normals.push_back(triplet<float, float, float>(0.0f, 0.0f, 0.0f));
+}
+
+Vertex::Vertex(float x, float y, float z)
+{
+        xyz.first = x;
+        xyz.second = y;
+        xyz.third = z;
 }
 
 Vertex::~Vertex()
 {
         //dtor
+}
+
+void Vertex::addNormal(float x, float y, float z)
+{
+        bool found = false;
+        for(std::vector<triplet<float, float, float> >::iterator it = normals.begin(); it != normals.end(); ++it)
+        {
+                triplet<float, float, float> normal = *it;
+                if(normal.first == x && normal.second == y && normal.third == z)
+                {
+                        found = true;
+                }
+        }
+
+        if(!found)
+        {
+                normals.push_back(triplet<float, float, float>(x, y, z));
+        }
+}
+
+void Vertex::setXyz(float x, float y, float z)
+{
+        xyz.first = 0.0f;
+        xyz.second = 0.0f;
+        xyz.third = 0.0f;
+}
+
+triplet<float, float, float> Vertex::getXyz()
+{
+        return xyz;
+}
+
+triplet<float, float, float> Vertex::getNormal()
+{
+        triplet<float, float, float> normal;
+
+        for(std::vector<triplet<float, float, float> >::iterator it = normals.begin(); it != normals.end(); ++it)
+        {
+                triplet<float, float, float> tmpNormal = *it;
+                normal.first += tmpNormal.first;
+                normal.second += tmpNormal.second;
+                normal.third += tmpNormal.third;
+        }
+
+        float length = sqrt((normal.first * normal. first) + (normal.second * normal.second) + (normal.third * normal.third));
+
+        normal.first /= length;
+        normal.second /= length;
+        normal.third /= length;
+
+        return normal;
 }
--- Vertex.h
+++ Vertex.h
@@ -1,14 +1,25 @@
 #ifndef VERTEX_H
 #define VERTEX_H
 
+#include <vector>
+#include "triplet.h"
 
 class Vertex
 {
         public:
                 Vertex();
+                Vertex(float x, float y, float z);
                 virtual ~Vertex();
+
+                triplet<float, float, float> getXyz();
+                triplet<float, float, float> getNormal();
+
+                void addNormal(float x, float y, float z);
+                void setXyz(float x, float y, float z);
         protected:
         private:
+                triplet<float, float, float> xyz;
+                std::vector<triplet<float, float, float> > normals;
 };
 
 #endif // VERTEX_H
