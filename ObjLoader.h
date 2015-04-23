7a5c8df323dc3423cfb4d80f7804c18506de03e5
Brock York <twunknown@gmail.com>
2015-04-23 19:51:18 +1000

    Added function declarations to allows texture coordinate loading

--- ObjLoader.h
+++ ObjLoader.h
@@ -17,6 +17,8 @@ class ObjLoader
                 size_t vsize();
                 float *getNormals();
                 size_t nsize();
+                float *getTexCoords();
+                size_t tsize();
 
                 float *getAvgNormals();
 
@@ -28,10 +30,12 @@ class ObjLoader
         private:
                 std::vector<Vertex> vertices;
                 std::vector<triplet<float, float, float> > normals;
+                std::vector<std::pair<float, float> > texCoords;
                 std::vector<Face> faces;
 
                 std::vector<float> loadNumbers(std::stringstream &ss);
                 std::vector<std::pair<int, bool> > loadFace(std::string &ss);
+                std::pair<float, float> loadTexCoords(std::stringstream &ss);
 };
 
 #endif // OBJLOADER_H
