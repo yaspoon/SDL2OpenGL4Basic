e4b8d78bf4a0a391baa941b738077ca6cfe03b92
Brock York <twunknown@gmail.com>
2015-08-24 16:52:10 +1000

    Now load waveform obj materials into one of these objects

---
+++ ObjMaterial.cpp
@@ -0,0 +1,72 @@
+#include "ObjMaterial.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+ObjMaterial::ObjMaterial(std::string filepath)
+{
+        std::ifstream matFile;
+        matFile.open(filepath.c_str(), std::ios::in);
+
+        for(std::string line; std::getline(matFile, line); /*void*/)
+        {
+                std::stringstream ss(line);
+                std::string identifier;
+
+                ss >> identifier;
+                if(identifier.compare("#") == 0)
+                {
+                        std::cout << "Found comment" << std::endl;
+                }
+                else if(identifier.compare("newmtl") == 0)
+                {
+                        ss >> name;
+                        std::cout << "Found name:" << name << std::endl;
+                }
+                else if(identifier.compare("Ns") == 0)
+                {
+                        ss >> ns;
+                        std::cout << "Ns:" << ns << std::endl;
+                }
+                else if(identifier.compare("Ka") == 0)
+                {
+                        for(int i = 0; i < 3; i++)
+                        {
+                                float tmp;
+                                ss >> tmp;
+                                ka.push_back(tmp);
+                        }
+                }
+                else if(identifier.compare("Kd") == 0)
+                {
+                        for(int i = 0; i < 3; i++)
+                        {
+                                float tmp;
+                                ss >> tmp;
+                                kd.push_back(tmp);
+                        }
+                }
+                else if(identifier.compare("Ks") == 0)
+                {
+                        for(int i = 0; i < 3; i++)
+                        {
+                                float tmp;
+                                ss >> tmp;
+                                ks.push_back(tmp);
+                        }
+                }
+                else if(identifier.compare("d") == 0)
+                {
+                        ss >> d;
+                }
+                else if(identifier.compare("illum") == 0)
+                {
+                        ss >> illum;
+                }
+                else if(identifier.compare("map_Kd") == 0)
+                {
+                        ss >> map_kd;
+                }
+        }
+}
---
+++ ObjMaterial.h
@@ -0,0 +1,23 @@
+#ifndef OBJ_MATERIAL_H
+#define OBJ_MATERIAL_H
+
+#include <string>
+#include <vector>
+
+class ObjMaterial
+{
+private:
+        std::string name;
+        int ns;
+        int d;
+        int illum;
+        std::vector<float> ka;
+        std::vector<float> kd;
+        std::vector<float> ks;
+        std::string map_kd;
+
+public:
+
+        ObjMaterial(std::string filepath);
+};
+#endif // OBJ_MATERIAL_H
