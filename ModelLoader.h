a635e0ef705a145cf4cd1f68e17ae7838b34baa2
Brock York <twunknown@gmail.com>
2015-09-25 21:47:20 +1000

    New abstract class that defines the methods the renderer needs to load a model

---
+++ ModelLoader.h
@@ -0,0 +1,26 @@
+#ifndef MODELLOADER_H
+#define MODELLOADER_H
+
+#include <stddef.h>
+#include "ObjMaterial.h"
+class ModelLoader
+{
+        public:
+                virtual ~ModelLoader() {}
+                virtual float *getVertices() = 0;
+                virtual size_t vsize() = 0;
+                virtual float *getNormals() = 0;
+                virtual size_t nsize() = 0;
+                virtual float *getTexCoords() = 0;
+                virtual size_t tsize() = 0;
+                virtual float *getColours() = 0;
+                virtual size_t csize() = 0;
+                virtual std::vector<ObjMaterial> getMaterials() = 0;
+                //virtual int *getIndices();
+                //virtual size_t isize();
+
+        protected:
+        private:
+};
+
+#endif // MODELLOADER_H
